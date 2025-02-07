299036090203cfae29858a9f8bcc35604db4421f
EasyBoyGame <easygameboyiphone@gmail.com>
2025-02-07 15:47:56 +0100

    adding sendPayment to all

--- src/main/java/dev/catalkaya/abiplaner/controller/BestellungController.java
+++ src/main/java/dev/catalkaya/abiplaner/controller/BestellungController.java
@@ -19,6 +19,8 @@ import org.eclipse.microprofile.jwt.Claims;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,7 +39,6 @@ public class BestellungController {
     QrCodeRepository qrCodeRepository;
     @Inject
     JsonWebToken jwt;
-
     @Inject
     Mailer mailer;
 
@@ -45,7 +46,16 @@ public class BestellungController {
     // alle vorhandenen Bestellungen abfragen
     @GET
     @RolesAllowed({"abiplaner-admin"})
-    public List<Bestellung> getBestellungen() {
+    public List<Bestellung> getBestellungen(){
+        try {
+            for (Bestellung bestellung : bestellungRepository.getBestellungen()) {
+                sendPaymentRequest(bestellung.benutzerEmail(), bestellung.anzahlEssenskarte(), bestellung.anzahlAbendkarte());
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new InternalServerErrorException();
+        }
+
         try {
             return bestellungRepository.getBestellungen();
         } catch (Exception ex) {
@@ -57,14 +67,13 @@ public class BestellungController {
 
     @PATCH
     @RolesAllowed({"abiplaner-admin"})
-    public void checkBestellung(UpdateBestellStatus status){
+    public void checkBestellung(UpdateBestellStatus status) {
         try {
-            if(bestellungRepository.existsBestellung(status.id())){
+            if (bestellungRepository.existsBestellung(status.id())) {
                 bestellungRepository.updateBestellStatus(status.id(), status.bezahlt());
                 sendQrCode(status.id());
             }
-        }
-        catch (SQLException ex){
+        } catch (SQLException ex) {
             ex.printStackTrace();
             throw new NotFoundException();
         }
@@ -76,29 +85,27 @@ public class BestellungController {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Authenticated
-    public Response newBestellung(BestellungRequest request){
+    public Response newBestellung(BestellungRequest request) {
         try {
             int anzahlEK = bestellungRepository.getAnzahlEK();
             int anzahlAK = bestellungRepository.getAnzahlAK();
-            if (!bestellungRepository.existsBenutzer(jwt.getSubject())){
-                if(request.anzahlEssenskarten() <= anzahlEK && request.anzahlAbendkarten() <= anzahlAK){
+            if (!bestellungRepository.existsBenutzer(jwt.getSubject())) {
+                if (request.anzahlEssenskarten() <= anzahlEK && request.anzahlAbendkarten() <= anzahlAK) {
                     bestellungRepository.newBestellung(jwt.getSubject(), jwt.getClaim(Claims.email), request.anzahlEssenskarten(), request.anzahlAbendkarten());
                     sendPaymentRequest(jwt.getClaim(Claims.email), request.anzahlEssenskarten(), request.anzahlAbendkarten());
                 } else {
                     return Response.status(Response.Status.BAD_REQUEST).build();
                 }
-            }
-            else {
-                if(request.anzahlEssenskarten() <= anzahlEK + bestellungRepository.getAnzahlEK(jwt.getSubject()) &&
-                        request.anzahlAbendkarten() <= anzahlAK + bestellungRepository.getAnzahlAK(jwt.getSubject())){
+            } else {
+                if (request.anzahlEssenskarten() <= anzahlEK + bestellungRepository.getAnzahlEK(jwt.getSubject()) &&
+                        request.anzahlAbendkarten() <= anzahlAK + bestellungRepository.getAnzahlAK(jwt.getSubject())) {
                     bestellungRepository.updateBestellung(jwt.getSubject(), request.anzahlEssenskarten(), request.anzahlAbendkarten(), false);
                     sendPaymentRequest(jwt.getClaim(Claims.email), request.anzahlEssenskarten(), request.anzahlAbendkarten());
                 } else {
                     return Response.status(Response.Status.BAD_REQUEST).build();
                 }
             }
-        }
-        catch (SQLException ex){
+        } catch (SQLException ex) {
             ex.printStackTrace();
             throw new InternalServerErrorException();
         }
@@ -130,8 +137,7 @@ public class BestellungController {
     }
 
 
-
-    public void sendPaymentRequest(String benutzerEmail, int anzahlEssenskarten, int anzahlAbendkarten){
+    public void sendPaymentRequest(String benutzerEmail, int anzahlEssenskarten, int anzahlAbendkarten) {
         String regex = "^(?:(?<vorname>[a-zA-Z]+)\\.)?(?<nachname>[a-zA-Z]+)@jsg-vechelde\\.de$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(benutzerEmail);
@@ -140,14 +146,13 @@ public class BestellungController {
         String vorname;
         String nachname;
 
-        if(matcher.matches()){
+        if (matcher.matches()) {
             vorname = matcher.group("vorname");
             nachname = matcher.group("nachname");
-        }
-        else return;
+        } else return;
         int summe = anzahlEssenskarten * 50 + anzahlAbendkarten * 20;
         //region if vorname != null
-        if(vorname != null && anzahlEssenskarten > 0 && anzahlAbendkarten > 0){
+        if (vorname != null && anzahlEssenskarten > 0 && anzahlAbendkarten > 0) {
             text = "Hallo " + capitilize(vorname) + " " + capitilize(nachname) + ",\n\n" +
                     "deine Bestellung über " + anzahlEssenskarten + " Essenskarten und " + anzahlAbendkarten + " Abendkarten ist bei uns eingegangen.\n" +
                     "Wir bitten dich den offenen Betrag von " + summe + "€ an das Konto mit der IBAN DE45 2505 0000 0202 0775 41 bis zum 21.02.2025 zu überweisen.\n\n" +
@@ -155,7 +160,7 @@ public class BestellungController {
                     "Vielen Dank für deine Bestellung! Wir freuen uns auf dich!\n\n" +
                     "Falls der offene Betrag bis zur Frist nicht beglichen wird, wird deine Bestellung automatisch gelöscht!" +
                     "Bei Problemen oder Änderungen mit deiner Bestellung, melde dich bitte beim Finanzkomitee.";
-        } else if(vorname != null && anzahlEssenskarten > 0 && anzahlAbendkarten == 0){
+        } else if (vorname != null && anzahlEssenskarten > 0 && anzahlAbendkarten == 0) {
             text = "Hallo " + capitilize(vorname) + " " + capitilize(nachname) + ",\n\n" +
                     "deine Bestellung über " + anzahlEssenskarten + " Essenskarten ist bei uns eingegangen.\n" +
                     "Wir bitten dich den offenen Betrag von " + summe + "€ an das Konto mit der IBAN DE45 2505 0000 0202 0775 41 bis zum 21.02.2025 zu überweisen.\n\n" +
@@ -163,7 +168,7 @@ public class BestellungController {
                     "Vielen Dank für deine Bestellung! Wir freuen uns auf dich!\n\n" +
                     "Falls der offene Betrag bis zur Frist nicht beglichen wird, wird deine Bestellung automatisch gelöscht!" +
                     "Bei Problemen oder Änderungen mit deiner Bestellung, melde dich bitte beim Finanzkomitee.";
-        } else if(vorname != null && anzahlEssenskarten == 0 && anzahlAbendkarten > 0){
+        } else if (vorname != null && anzahlEssenskarten == 0 && anzahlAbendkarten > 0) {
             text = "Hallo " + capitilize(vorname) + " " + capitilize(nachname) + ",\n\n" +
                     "deine Bestellung über " + anzahlAbendkarten + " Abendkarten ist bei uns eingegangen.\n" +
                     "Wir bitten dich den offenen Betrag von " + summe + "€ an das Konto mit der IBAN DE45 2505 0000 0202 0775 41 bis zum 21.02.2025 zu überweisen.\n\n" +
@@ -174,7 +179,7 @@ public class BestellungController {
         }
         //endregion
         //region if vorname == null
-        if(vorname == null && anzahlEssenskarten > 0 && anzahlAbendkarten > 0){
+        if (vorname == null && anzahlEssenskarten > 0 && anzahlAbendkarten > 0) {
             text = "Hallo Herr/Frau " + capitilize(nachname) + ",\n\n" +
                     "Ihre Bestellung über " + anzahlEssenskarten + " Essenskarten und " + anzahlAbendkarten + " Abendkarten ist bei uns eingegangen.\n" +
                     "Wir bitten Sie den offenen Betrag von " + summe + "€ an das Konto mit der IBAN DE45 2505 0000 0202 0775 41 bis zum 21.02.2025 zu überweisen.\n\n" +
@@ -182,7 +187,7 @@ public class BestellungController {
                     "Vielen Dank für Ihre Bestellung! Wir freuen uns auf Sie!\n\n" +
                     "Falls der offene Betrag bis zur Frist nicht beglichen wird, wird Ihre Bestellung automatisch gelöscht!" +
                     "Bei Problemen oder Änderungen mit Ihrer Bestellung, melden Sie sich bitte beim Finanzkomitee.";
-        } else if(vorname != null && anzahlEssenskarten > 0 && anzahlAbendkarten == 0){
+        } else if (vorname != null && anzahlEssenskarten > 0 && anzahlAbendkarten == 0) {
             text = "Hallo Herr/Frau " + capitilize(nachname) + ",\n\n" +
                     "Ihre Bestellung über " + anzahlEssenskarten + " Essenskarten ist bei uns eingegangen.\n" +
                     "Wir bitten Sie den offenen Betrag von " + summe + "€ an das Konto mit der IBAN DE45 2505 0000 0202 0775 41 bis zum 21.02.2025 zu überweisen.\n\n" +
@@ -190,7 +195,7 @@ public class BestellungController {
                     "Vielen Dank für Ihre Bestellung! Wir freuen uns auf Sie!\n\n" +
                     "Falls der offene Betrag bis zur Frist nicht beglichen wird, wird Ihre Bestellung automatisch gelöscht!" +
                     "Bei Problemen oder Änderungen mit Ihrer Bestellung, melden Sie sich bitte beim Finanzkomitee.";
-        } else if(vorname != null && anzahlEssenskarten == 0 && anzahlAbendkarten > 0){
+        } else if (vorname != null && anzahlEssenskarten == 0 && anzahlAbendkarten > 0) {
             text = "Hallo Herr/Frau " + capitilize(nachname) + ",\n\n" +
                     "Ihre Bestellung über " + anzahlAbendkarten + " Abendkarten ist bei uns eingegangen.\n" +
                     "Wir bitten Sie den offenen Betrag von " + summe + "€ an das Konto mit der IBAN DE45 2505 0000 0202 0775 41 bis zum 21.02.2025 zu überweisen.\n\n" +
@@ -221,41 +226,40 @@ public class BestellungController {
             String vorname;
             String nachname;
 
-            if(matcher.matches()){
+            if (matcher.matches()) {
                 vorname = matcher.group("vorname");
                 nachname = matcher.group("nachname");
-            }
-            else return;
+            } else return;
 
             //region if vorname != null
-            if(vorname != null && bestellung.anzahlEssenskarte() > 0 && bestellung.anzahlAbendkarte() > 0){
-                text = "Hallo " + capitilize(vorname) + " " + capitilize(nachname) + ",\n\n"+
+            if (vorname != null && bestellung.anzahlEssenskarte() > 0 && bestellung.anzahlAbendkarte() > 0) {
+                text = "Hallo " + capitilize(vorname) + " " + capitilize(nachname) + ",\n\n" +
                         "im Anhang befinden sich deine Eintrittskarte(n) für den Abiball!\n\n" +
                         "Die Karte(n) bis " + bestellung.anzahlEssenskarte() + "-" + bestellung.id() + " ist/sind die Essenskarte(n). Die restlichen Karte(n) sind die Abendkarte(n) für die Aftershow.\n\n" +
                         "Der reguläre Eintritt beginnt um 18:00 Uhr, die Aftershow beginnt ab 22:00 Uhr.";
-            } else if(vorname != null && bestellung.anzahlEssenskarte() > 0 && bestellung.anzahlAbendkarte() == 0){
-                text = "Hallo " + capitilize(vorname) + " " + capitilize(nachname) + ",\n\n"+
+            } else if (vorname != null && bestellung.anzahlEssenskarte() > 0 && bestellung.anzahlAbendkarte() == 0) {
+                text = "Hallo " + capitilize(vorname) + " " + capitilize(nachname) + ",\n\n" +
                         "im Anhang befinden sich deine Essenskarte(n). Der Eintritt findet ab 18:00 Uhr statt.\n\n\n";
             } else if (vorname != null && bestellung.anzahlEssenskarte() == 0 && bestellung.anzahlAbendkarte() > 0) {
-                text = "Hallo " + capitilize(vorname) + " " + capitilize(nachname) + ",\n\n"+
+                text = "Hallo " + capitilize(vorname) + " " + capitilize(nachname) + ",\n\n" +
                         "im Anhang befinden sich deine Abendkarte(n) für die Aftershow ab 22:00 Uhr.";
             }
             //endregion
             //region if vorname == null
-            if(vorname == null && bestellung.anzahlEssenskarte() > 0 && bestellung.anzahlAbendkarte() > 0){
-                text = "Hallo Herr/Frau " + capitilize(nachname) + ",\n\n"+
+            if (vorname == null && bestellung.anzahlEssenskarte() > 0 && bestellung.anzahlAbendkarte() > 0) {
+                text = "Hallo Herr/Frau " + capitilize(nachname) + ",\n\n" +
                         "im Anhang befinden sich Ihre Eintrittskarte(n) für den Abiball!\n\n" +
                         "Die Karte(n) bis " + bestellung.anzahlEssenskarte() + "-" + bestellung.id() + " ist/sind die Essenskarte(n). Die restlichen Karte(n) sind die Abendkarte(n) für die Aftershow.\n\n" +
                         "Der reguläre Eintritt beginnt um 18:00 Uhr, die Aftershow beginnt ab 22:00 Uhr.";
-            } else if(vorname == null && bestellung.anzahlEssenskarte() > 0 && bestellung.anzahlAbendkarte() == 0){
-                text = "Hallo Herr/Frau " + capitilize(nachname) + ",\n\n"+
+            } else if (vorname == null && bestellung.anzahlEssenskarte() > 0 && bestellung.anzahlAbendkarte() == 0) {
+                text = "Hallo Herr/Frau " + capitilize(nachname) + ",\n\n" +
                         "im Anhang befinden sich Ihre Essenskarte(n). Der Eintritt findet ab 18:00 Uhr statt.\n\n\n";
             } else if (vorname == null && bestellung.anzahlEssenskarte() == 0 && bestellung.anzahlAbendkarte() > 0) {
-                text = "Hallo Herr/Frau " + capitilize(nachname) + ",\n\n"+
+                text = "Hallo Herr/Frau " + capitilize(nachname) + ",\n\n" +
                         "im Anhang befinden sich Ihre Abendkarte(n) für die Aftershow ab 22:00 Uhr.";
             }
             //endregion
-            text += "\n\n\nViel Spaß beim Abiball!\n\n\n\n\n"+ "Adresse:\n" + "Ilseder Hütte 14,\n31241 Ilsede,\nDeutschland" +
+            text += "\n\n\nViel Spaß beim Abiball!\n\n\n\n\n" + "Adresse:\n" + "Ilseder Hütte 14,\n31241 Ilsede,\nDeutschland" +
                     "\n\n\n\nDies ist eine automatisch generierte E-Mail auf die nicht geantwortet werden soll!";
 
             //String path = "D:/Dokumente/Schule/2_EK_Informatik/AbiplanerQuark/abiplaner/build/classes/java/main/output/" + bestellung.id();
@@ -264,21 +268,20 @@ public class BestellungController {
             File[] files = dir.listFiles();
 
             ArrayList<Attachment> attachments = new ArrayList<>();
-            for(File file: files){
+            for (File file : files) {
                 attachments.add(new Attachment(file.getName(), file, "image/jpg"));
             }
 
-            mailer.send(Mail.withText(bestellung.benutzerEmail(), "Abiballkarten!",text).setAttachments(attachments));
+            mailer.send(Mail.withText(bestellung.benutzerEmail(), "Abiballkarten!", text).setAttachments(attachments));
 
             bestellungRepository.mailSend(bestellung.id());
-        }
-        catch (SQLException ex){
+        } catch (SQLException ex) {
             ex.printStackTrace();
         }
     }
 
 
-    public String capitilize(String input){
+    public String capitilize(String input) {
         return Character.toUpperCase(input.charAt(0)) + input.substring(1);
     }
 }
