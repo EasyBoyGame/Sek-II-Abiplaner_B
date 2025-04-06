1f9ab88c2729c156b9a45a39291140d1721fdb7c
EasyBoyGame <easygameboyiphone@gmail.com>
2025-04-06 19:01:08 +0200

    update checkinStatus

--- src/main/java/dev/catalkaya/abiplaner/controller/QrCodeController.java
+++ src/main/java/dev/catalkaya/abiplaner/controller/QrCodeController.java
@@ -1,17 +1,14 @@
 package dev.catalkaya.abiplaner.controller;
 
-import dev.catalkaya.abiplaner.model.SessionStatus;
 import dev.catalkaya.abiplaner.repository.QrCodeRepository;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-import java.lang.annotation.Repeatable;
+import java.net.URI;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.sql.SQLException;
 
 
 @Path("/api/v1/checkin")
@@ -20,31 +17,24 @@ public class QrCodeController {
     @Inject
     QrCodeRepository qrCodeRepository;
 
-    @Inject
-    SessionStatus sessionStatus;
-
     @GET
     @Consumes(MediaType.TEXT_PLAIN)
     public Response checkInCustomer(@QueryParam("kartenNr") String kartenNr){
         if(kartenNr == null || kartenNr.isEmpty()){
-            sessionStatus.setCheckinStatus("failure");
-            return Response
-                    .seeOther(java.net.URI.create("https://abiplaner.catalkaya.dev/checkin"))
-                    .build();
+            return createRedirectResponse("failure");
             //return Response.status(Response.Status.BAD_REQUEST).entity("Missing kartenNr").build();
         }
 
         boolean valid = qrCodeRepository.validateQrCode(URLDecoder.decode(kartenNr, StandardCharsets.UTF_8));
-        if(valid){
-            sessionStatus.setCheckinStatus("success");
-            return Response
-                    .seeOther(java.net.URI.create("https://abiplaner.catalkaya.dev/checkin"))
-                    .build();
-        } else {
-            sessionStatus.setCheckinStatus("failure");
-            return Response
-                    .seeOther(java.net.URI.create("https://abiplaner.catalkaya.dev/checkin"))
-                    .build();
-        }
+        return createRedirectResponse(valid ? "success" : "failure");
+    }
+
+    private Response createRedirectResponse(String status) {
+        String redirectUrl = "/checkin/" + status;
+        String cookieValue = String.format("checkinStatus=%s; Path=/checkin; Max-Age=60; HttpOnly; SameSite=Strict", status);
+        return Response
+                .seeOther(URI.create(redirectUrl))
+                .header("Set-Cookie", cookieValue)
+                .build();
     }
 }
